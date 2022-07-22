a4586318f5a124af9bb24704e8e0ccb9fc7da1f0
Armand Piers <armand.piers@outlook.com>
2022-07-22 17:32:19 +0200

    Mail functionality to send emails via Go
    Mail package provides mailing functionality using GoMail package in order to send emails using a defined server

---
+++ mail/mail.go
@@ -0,0 +1,82 @@
+package mail
+
+import (
+	"fmt"
+	"strings"
+
+	"gopkg.in/gomail.v2"
+)
+
+// OptionsFn is the option function type
+type OptionsFn func(*Server)
+
+// WithoutSSL disables SSL check for email server
+func WithoutSSL() OptionsFn {
+	return func(s *Server) {
+		s.SSL = false
+	}
+}
+
+// SkipSSLVerify will not verify emial server certificate
+func SkipSSLVerify() OptionsFn {
+	return func(s *Server) {
+		s.TLSConfig.InsecureSkipVerify = true
+	}
+}
+
+// Server represents the email server
+type Server struct {
+	*gomail.Dialer
+}
+
+// Message represents the email details
+type Message struct {
+	From     string
+	To       []string
+	Subject  string
+	BodyText string
+	BodyHTML string
+}
+
+// NewServer creates new Mail server connection
+func NewServer(options ...OptionsFn) (s *Server) {
+	s = &Server{}
+
+	for _, optionFn := range options {
+		optionFn(s)
+	}
+
+	return s
+}
+
+// Send function will send a message via the server using the Message details
+func (s *Server) Send(m *Message) error {
+
+	message := gomail.NewMessage()
+
+	// From address
+	message.SetHeader("From", m.From)
+
+	// To address/recipients
+	// m.SetAddressHeader("To", r.Address, r.Name)
+	message.SetHeader("To", strings.Join(m.To, ";"))
+
+	// Subject of message
+	message.SetHeader("Subject", m.Subject)
+
+	// Body of message
+	if m.BodyText != "" {
+		message.SetBody("text/plain", m.BodyText)
+	} else {
+		message.SetBody("text/html", m.BodyHTML)
+	}
+
+	// Dailer to send message
+	// d := gomail.NewDialer(s.Host, s.Port, s.Username, s.Password)
+
+	// Send the message using the dailer.
+	if err := s.DialAndSend(message); err != nil {
+		return fmt.Errorf("Unable to send message: %s", err)
+	}
+	return nil
+}
